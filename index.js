b367c5bd5a37463c6fefd2f8ed0c41a2cdbb8ee0
Nicolás Ludueña <luduena.nicolas.victorio@gmail.com>
2022-02-09 00:08:03 -0300

    better load init behavior

--- index.js
+++ index.js
@@ -9,15 +9,8 @@ import {ToadScheduler} from 'toad-scheduler';
 import {refreshStarlink} from './controllers/utils.js';
 
 
-const app = express();
 dotenv.config();
 
-
-app.use(bodyParser.json({limit: '30mb', extended: true}));
-app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
-app.use(cors());
-
-app.use('/satellite', satelliteRoutes);
 
 const CONNECTION_URL = process.env.MONGO_URI;
 const PORT = process.env.PORT || 5000;
@@ -25,10 +18,17 @@ const PORT = process.env.PORT || 5000;
 
 const scheduler = new ToadScheduler();
 addScheduledJobs(scheduler);
-refreshStarlink();
 
 mongoose.connect(
     CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(
         PORT, () => console.log(`Server running on port: ${PORT}`)),
     ).catch((error) => console.log(error.message));
+
+refreshStarlink();
+const app = express();
+app.use(bodyParser.json({limit: '30mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+app.use(cors());
+
+app.use('/satellite', satelliteRoutes);
