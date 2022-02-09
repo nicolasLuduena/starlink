acad31f747598a65de0f97c83ae849cba40eba84
Nicolás Ludueña <luduena.nicolas.victorio@gmail.com>
2022-02-09 00:17:03 -0300

    fix initial behavior on data loading

--- index.js
+++ index.js
@@ -19,14 +19,16 @@ const PORT = process.env.PORT || 5000;
 const scheduler = new ToadScheduler();
 addScheduledJobs(scheduler);
 
+const app = express();
 mongoose.connect(
     CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(
-        PORT, () => console.log(`Server running on port: ${PORT}`)),
+    .then(async () => {
+      await refreshStarlink();
+      app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+    },
     ).catch((error) => console.log(error.message));
 
-refreshStarlink();
-const app = express();
+
 app.use(bodyParser.json({limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
 app.use(cors());
